6b82a3aaf49c8edbddb1d101b674fd6b33d1e19e
Anthony Camacho <anthony.g.camacho@gmail.com>
2023-07-21 15:21:28 -0700

    github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://anthonygcamacho.github.io",
+    base: "/astro-boilerplate",
     integrations: [tailwind({ applyBaseStyles: false })],
 })
